36a1d50cad12efd05215543d3dd2b80f0c8c4faf
exploitenomah <iksploiting@gmail.com>
2023-03-04 17:28:30 +0100

    add styles to body

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-bg-default bg-no-repeat">
+      <body className="bg-primary-default bg-no-repeat text-contrast-primary">
         <Main />
         <NextScript />
       </body>

0ed642c1366b7e537bd11585bd8843ee25d6d84e
exploitenomah <iksploiting@gmail.com>
2023-03-04 17:31:44 +0100

    create signup form

---
+++ components/Custom/Auth/Signup.tsx
@@ -0,0 +1,34 @@
+import Button from '@components/HTML/Button'
+import Input from '@components/HTML/Input'
+import LoadingLogo from '../../../assets/LoadingLogo'
+import Modal from '../Modal'
+import FormContainer from './FormContainer'
+
+export default function Signup({
+  show,
+  hide,
+  switchToLogin,
+}: {
+  show: boolean
+  hide: () => void
+  switchToLogin: () => void
+}) {
+  return (
+    <FormContainer
+      show={show}
+      hide={hide}
+      toggle={switchToLogin}
+      mode='sign up'
+      onSubmit={(e) => ({})}
+      title='Get Started With ChatsApp'
+    >
+      <div className='flex gap-6 justify-between'>
+        <Input placeholder='first name*' required />
+        <Input placeholder='last name' />
+      </div>
+      <Input placeholder='nick name*' required />
+      <Input placeholder='email*' required />
+      <Input placeholder='password*' required />
+    </FormContainer>
+  )
+}

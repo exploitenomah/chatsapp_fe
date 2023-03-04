c3721b0bc4bd8bcd230efd0bef0d095a842e25e9
exploitenomah <iksploiting@gmail.com>
2023-03-04 19:45:31 +0100

    formatting

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
-      <body className="bg-primary-default bg-no-repeat text-contrast-primary">
+      <body className='bg-primary-default bg-no-repeat text-contrast-primary'>
+        <div className='body-bg bg-primary-default'></div>
         <Main />
         <NextScript />
       </body>

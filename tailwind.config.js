0749be64dbcbb07bd2dc4e5f629364b9a1b0648c
exploitenomah <iksploiting@gmail.com>
2023-03-04 03:29:10 +0100

    setup config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
 
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: {
+          default: '#111b21',
+          darkest: '#0b141a',
+          dark: '#0a1014',
+          medium: '#182229',
+        },
+        accent: {
+          dark: '#00a884',
+          medium: '#06cf9c',
+          light: '#71eb85',
+          bright: '#09d261',
+        },
+        text: { primary: '#e9edef', secondary: '#8696a0' },
+      },
+    },
   },
   plugins: [],
-};
+}

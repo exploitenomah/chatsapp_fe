e47e42af7a26192f4798b1bcc340615053b0c437
exploitenomah <iksploiting@gmail.com>
2023-03-04 17:27:50 +0100

    add auth modals to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,57 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import dynamic from 'next/dynamic'
+import { useState, useEffect, useCallback } from 'react'
+
+const Login = dynamic(() => import('@components/Custom/Auth/Login'), {
+  ssr: false,
+})
+const Signup = dynamic(() => import('@components/Custom/Auth/Signup'), {
+  ssr: false,
+})
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [showSignup, setShowSignup] = useState(false)
+  const [showLogin, setShowLogin] = useState(false)
+  const [token, setToken] = useState<string | null>(null)
+
+  const hideSignupShowLogin = useCallback(() => {
+    setShowSignup(false)
+    setShowLogin(true)
+  }, [])
+
+  const hideLoginShowSignup = useCallback(() => {
+    setShowSignup(true)
+    setShowLogin(false)
+  }, [])
+
+  const hideSignupAndLogin = useCallback(() => {
+    setShowLogin(false)
+    setShowSignup(false)
+  }, [])
+
+  useEffect(() => {
+    let tokenInLs = window.localStorage.getItem('token')
+    if (tokenInLs !== null) {
+      setToken(tokenInLs)
+      setShowLogin(false)
+      setShowSignup(false)
+    }
+  }, [])
+
   return (
-    <>
+    <div>
       <Component {...pageProps} />
-    </>
+      <Signup
+        show={showSignup}
+        hide={hideSignupAndLogin}
+        switchToLogin={hideSignupShowLogin}
+      />
+      <Login
+        show={showLogin}
+        hide={hideSignupAndLogin}
+        switchToLogin={hideLoginShowSignup}
+      />
+    </div>
   )
 }

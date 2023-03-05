317b7f4936dd7bcacd26e817c72baa11bcee53b2
exploitenomah <iksploiting@gmail.com>
2023-03-05 14:08:12 +0100

    give input component overridable classes

--- components/HTML/Input.tsx
+++ components/HTML/Input.tsx
@@ -8,8 +8,7 @@ export default function Input({
 }: InputHTMLAttributes<HTMLInputElement>) {
   return (
     <input
-      className={`inline-block w-full bg-secondary-darkest text-contrast-secondary 
-      focus:outline-none placeholder:text-contrast-secondary placeholder:capitalize px-4 pt-3 pb-4 rounded-lg ${className}`}
+      className={`input-default ${className}`}
       onChange={onChange}
       value={value}
       {...others}

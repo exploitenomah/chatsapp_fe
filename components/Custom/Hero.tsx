e57bb972afd4d2142d098f78c18c977c95872ce2
exploitenomah <iksploiting@gmail.com>
2023-03-05 14:07:23 +0100

    extract app intro to separate component

--- components/Custom/Hero.tsx
+++ components/Custom/Hero.tsx
@@ -1,6 +1,7 @@
 import DarkIntroLogo from '@/assets/DarkIntroLogo'
 import Button from '@components/HTML/Button'
 import { Roboto, Dancing_Script } from 'next/font/google'
+import AppIntro from './AppIntro'
 
 const roboto = Roboto({
   weight: '400',
@@ -20,7 +21,7 @@ export default function Hero({
   showSignup: () => void
 }) {
   return (
-    <div className=' flex flex-col h-screen w-screen justify-center  '>
+    <div className=' flex flex-col h-screen w-screen justify-center border-x-accent-darkest border-x-4'>
       <header className='flex justify-between items-center w-[80vw] max-w-[1000px] mx-auto pt-8 mb-auto'>
         <h1 className={`${dancingScript.className} text-md weight-400`}>
           ChatsApp Web
@@ -34,11 +35,7 @@ export default function Hero({
       </header>
       <main className={`${roboto.className} text-center mb-auto`}>
         <div className='px-4'>
-          <DarkIntroLogo className='max-w-[290px] md:max-w-[initial] inline-block mx-auto' />
-          <h1 className='text-3xl weight-300 mt-10'>ChatsApp Web</h1>
-          <p className='mt-4 text-sm text-contrast-secondary'>
-            Send and recieve messages in your web browser.
-          </p>
+          <AppIntro />
           <Button
             className='bg-accent-darkest text-white mt-4'
             onClick={showSignup}

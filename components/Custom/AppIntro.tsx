2b02e828191f459b4b7458578341b95688113e3e
exploitenomah <iksploiting@gmail.com>
2023-03-05 14:12:05 +0100

    extract app intro to separate component

---
+++ components/Custom/AppIntro.tsx
@@ -0,0 +1,14 @@
+import DarkIntroLogo from '@/assets/DarkIntroLogo'
+import Button from '@components/HTML/Button'
+
+export default function AppIntro() {
+  return (
+    <>
+      <DarkIntroLogo className='max-w-[290px] md:max-w-[initial] inline-block mx-auto text-center' />
+      <h1 className='text-3xl weight-300 mt-10 text-center'>ChatsApp Web</h1>
+      <p className='mt-4 text-sm text-contrast-secondary text-center'>
+        Send and recieve messages in your web browser.
+      </p>
+    </>
+  )
+}

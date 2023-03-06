47c14264dee44fca286a6fac90303059f7a46cf1
exploitenomah <iksploiting@gmail.com>
2023-03-06 10:26:33 +0100

    export all assets

---
+++ assets/index.tsx
@@ -0,0 +1,25 @@
+import CloseIcon from './CloseIcon'
+import DarkIntroLogo from './DarkIntroLogo'
+import EmojiIcon from './EmojiIcon'
+import FriendsIcon from './FriendsIcon'
+import LeftArrow from './LeftArrow'
+import LoadingLogo from './LoadingLogo'
+import MsgBubbleIcon from './MsgBubbleIcon'
+import OptionsIcon from './OptionsIcon'
+import SearchIcon from './SearchIcon'
+import SendIcon from './SendIcon'
+import FileInputIcon from './FileInputIcon'
+
+export {
+  CloseIcon,
+  DarkIntroLogo,
+  EmojiIcon,
+  FriendsIcon,
+  LeftArrow,
+  LoadingLogo,
+  MsgBubbleIcon,
+  OptionsIcon,
+  SearchIcon,
+  SendIcon,
+  FileInputIcon,
+}

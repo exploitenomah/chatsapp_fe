33b9e88e06740b63ab6af4ae9488fccee85f5322
exploitenomah <iksploiting@gmail.com>
2023-03-07 08:24:11 +0100

    fix misspelled jsx attributes

--- assets/SendIcon.tsx
+++ assets/SendIcon.tsx
@@ -11,7 +11,7 @@ export default function SendIcon({ className }: HTMLAttributes<SVGElement>) {
       version='1.1'
       x='0px'
       y='0px'
-      enable-background='new 0 0 24 24'
+      enableBackground='new 0 0 24 24'
       xmlSpace='preserve'
     >
       <path

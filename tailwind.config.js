bc8d8d7619d717acc0d729dd6541be8ff83766d6
exploitenomah <iksploiting@gmail.com>
2023-03-08 12:13:22 +0100

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,6 +39,7 @@ module.exports = {
         contrast: {
           primary: '#d1d7db',
           secondary: '#8696a0',
+          tetiary: '#d9dee0',
           strong: '#e9edef',
           light: '#222e35',
         },

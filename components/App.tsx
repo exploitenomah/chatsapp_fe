89deaafcb01a2b4a779777a6bfb980705eefab83
exploitenomah <iksploiting@gmail.com>
2023-03-08 12:00:19 +0100

    add friends drawer to left section

--- components/App.tsx
+++ components/App.tsx
@@ -1,4 +1,5 @@
 import { PageProps } from '../types/PageProps'
+import FriendsDrawer from './Custom/Friends/FriendsDrawer'
 import LeftPanel from './LeftPanel'
 import RightPanel from './RightPanel'
 
@@ -6,7 +7,8 @@ export default function App({}: PageProps) {
   return (
     <div className='w-screen h-screeen bg-primary-default'>
       <div className='flex max-w-[1600px] mx-auto h-screen 2xl:py-[19px]'>
-        <div className='basis-[30%] max-w-[30%]'>
+        <div className='basis-[30%] max-w-[30%] relative overflow-x-hidden'>
+          <FriendsDrawer />
           <LeftPanel />
         </div>
         <div className='grow border-l border-l-contrast-secondary/20'>

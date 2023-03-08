a0461820b580c8cf76cc8acd4d6757c622af7f4c
exploitenomah <iksploiting@gmail.com>
2023-03-08 12:01:49 +0100

    pass placeholder prop to searchbar

--- components/Custom/AppLeftBody.tsx
+++ components/Custom/AppLeftBody.tsx
@@ -4,9 +4,9 @@ export default function AppLeftBody() {
   return (
     <>
       <div className='relative app-left-body'>
-        <div className='sticky top-0 w-full'>
+        <div className='sticky top-0 w-full z-10'>
           <div className='w-11/12 mx-auto py-2'>
-            <SearchBar />
+            <SearchBar placeholder='Search or start a new chat' />
           </div>
         </div>
         <div className='absolute bottom-0 w-full top-[52px] overflow-auto'>

02fd9ddbb80ce1fc1e2dbeaa5e581c06fc6d9903
exploitenomah <iksploiting@gmail.com>
2023-03-09 06:05:50 +0100

    set up ui initial state

---
+++ store/ui/initialState.ts
@@ -0,0 +1,10 @@
+const initialState = {
+  showFriendsDrawer: false,
+  showFriendsSuggestionDrawer: false,
+  showLogin: false,
+  showSignup: false,
+}
+
+export type UI = typeof initialState
+
+export default initialState

c21e37458b0173526956fbfbd318b3473eef382d
exploitenomah <iksploiting@gmail.com>
2023-03-09 05:59:32 +0100

    control form inputs

--- components/Custom/Auth/Login.tsx
+++ components/Custom/Auth/Login.tsx
@@ -1,26 +1,81 @@
 import Input from '@components/HTML/Input'
 import FormContainer from './FormContainer'
+import { useState, useCallback, ChangeEvent, FormEvent } from 'react'
+import Button from '@components/HTML/Button'
+import { Store } from '@store/index'
+import { UI } from '@store/ui/initialState'
+import { useSelector } from 'react-redux'
 
-export default function Login({
-  show,
-  hide,
-  switchToLogin,
+const initialLoginDetails = {
+  nickNameOrEmail: '',
+  password: '',
+}
+
+type LoginDetails = typeof initialLoginDetails
+
+const Form = ({
+  handleSubmit,
 }: {
-  show: boolean
-  hide: () => void
-  switchToLogin: () => void
-}) {
+  handleSubmit: (loginDetails: LoginDetails) => void
+}) => {
+  const [loginDetails, setLoginDetails] = useState(initialLoginDetails)
+
+  const handleFormChange = useCallback(
+    (changeEvent: ChangeEvent<HTMLInputElement>) => {
+      setLoginDetails((prev) => ({
+        ...prev,
+        [changeEvent.target.name]: changeEvent.target.value,
+      }))
+    },
+    [],
+  )
+
+  const onSubmit = useCallback(
+    (submitEvent: FormEvent) => {
+      submitEvent.preventDefault()
+      handleSubmit(loginDetails)
+    },
+    [handleSubmit, loginDetails],
+  )
+
   return (
-    <FormContainer
-      show={show}
-      hide={hide}
-      toggle={switchToLogin}
-      mode='login'
-      onSubmit={(e) => ({})}
-      title='Welcome Back!'
-    >
-      <Input placeholder='nick-name/email*' required />
-      <Input placeholder='password*' required />
+    <form className='flex flex-col gap-5' onSubmit={onSubmit}>
+      <Input
+        placeholder='Nick Name / Email*'
+        required
+        type='text'
+        name='nickNameOrEmail'
+        value={loginDetails.nickNameOrEmail}
+        onChange={handleFormChange}
+      />
+      <Input
+        placeholder='Password*'
+        required
+        type='password'
+        name='password'
+        value={loginDetails.password}
+        onChange={handleFormChange}
+      />
+      <Button
+        type='submit'
+        name='login'
+        className='bg-accent-dark/60 mt-2 capitalize text-lg'
+      >
+        Login
+      </Button>
+    </form>
+  )
+}
+
+export default function Login() {
+  const { showLogin } = useSelector<Store, UI>((store) => store.ui)
+  const handleSubmit = useCallback((loginDetails: LoginDetails) => {
+    console.log(loginDetails)
+  }, [])
+
+  return (
+    <FormContainer show={showLogin} mode='login' title='Welcome Back!'>
+      <Form handleSubmit={handleSubmit} />
     </FormContainer>
   )
 }

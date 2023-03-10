8a061812cd137c829f4e545fa246c900bff18bcc
exploitenomah <iksploiting@gmail.com>
2023-03-10 12:05:39 +0100

    correct config

--- components/Custom/InactiveRightPanel.tsx
+++ components/Custom/InactiveRightPanel.tsx
@@ -3,7 +3,7 @@ import AppIntro from './AppIntro'
 export default function InactiveRightPanel() {
   return (
     <>
-      <div className='bg-contrast-light h-full flex justify-center items-center border-b-4 border-b-accent-dark'>
+      <div className='bg-primary-light h-full flex justify-center items-center border-b-4 border-b-accent-dark'>
         <div>
           <AppIntro />
         </div>

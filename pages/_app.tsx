646655dd960cd30921d1f64daecad669c8955f9f
exploitenomah <iksploiting@gmail.com>
2023-03-10 15:48:41 +0100

    remove saved form values before page unload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,27 @@ import { useState, useEffect } from 'react'
 import store from '@store/index'
 import { Provider } from 'react-redux'
 
+const variablesToDeleteFromLocalStorage = [
+  'firstName',
+  'lastName',
+  'email',
+  'password',
+  'nickName',
+  'confirmPassword',
+  'nickNameOrEmail',
+]
+
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    window.addEventListener('beforeunload', (ev) => {
+      variablesToDeleteFromLocalStorage.forEach((el) =>
+        localStorage.removeItem(el),
+      )
+    })
+    return () => {
+      window.removeEventListener('beforeunload', (ev) => {})
+    }
+  }, [])
   return (
     <Provider store={store}>
       <div>

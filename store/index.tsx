5ab3439f99a36dd5c5dc37cc5e70d7d9cdcd3032
exploitenomah <iksploiting@gmail.com>
2023-03-10 12:08:28 +0100

    add auth and notification reducers to store

--- store/index.tsx
+++ store/index.tsx
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import ui from './ui/slice'
+import auth from './auth/slice'
+import notification from './notifications/slice'
 
 const store = configureStore({
-  reducer: { ui },
+  reducer: { ui, auth, notification },
 })
 const state = store.getState()
 export type Store = typeof state

9f3fecf434a5f5e0f3425a7986536f19e3d61756
exploitenomah <iksploiting@gmail.com>
2023-03-10 15:46:46 +0100

    add notifications to app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import AppHead from '@components/Custom/Head'
-import Hero from '@components/Custom/Hero'
-import App from '@components/App'
+import Hero from '@components/Custom/App/Hero'
+import App from '@components/Custom/App/App'
 import useRoot from '@sockets/useRoot'
 import { Socket } from 'socket.io-client'
 import { useCallback, useEffect } from 'react'
@@ -9,7 +9,9 @@ import { useSelector } from 'react-redux'
 import { Store } from '@store/index'
 import { Auth } from '@store/auth/initialState'
 import { UI } from '@store/ui/initialState'
-import AppLoadingScreen from '@components/LoadingScreen'
+import AppLoadingScreen from '@components/Custom/App/LoadingScreen'
+import { Notifications } from '@store/notifications/initialState'
+import NotificationComponent from '@components/Custom/Notification'
 
 const Login = dynamic(() => import('@components/Custom/Auth/Login'), {
   ssr: false,
@@ -17,6 +19,20 @@ const Login = dynamic(() => import('@components/Custom/Auth/Login'), {
 const Signup = dynamic(() => import('@components/Custom/Auth/Signup'), {
   ssr: false,
 })
+
+const AppNotifications = () => {
+  const { appAlerts } = useSelector<Store, Notifications>(
+    (store) => store.notification,
+  )
+  console.log(appAlerts)
+  return (
+    <div className='absolute top-0 right-0 w-[25%]'>
+      {appAlerts.map((appAlert) => (
+        <NotificationComponent key={appAlert.id} {...appAlert} />
+      ))}
+    </div>
+  )
+}
 
 export default function Home() {
   const { token } = useSelector<Store, Auth>((store) => store.auth)
@@ -41,7 +57,13 @@ export default function Home() {
   })
 
   if (appLoading) return <AppLoadingScreen />
-  if (token) return <App token={token} />
+  if (token)
+    return (
+      <>
+        <AppNotifications />
+        <App />
+      </>
+    )
 
   return (
     <>
@@ -49,6 +71,7 @@ export default function Home() {
       <Hero />
       <Signup rootSocket={rootSocket} />
       <Login rootSocket={rootSocket} />
+      <AppNotifications />
     </>
   )
 }

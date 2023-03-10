7602f136c6633ddf644df3daef1422f949e570b9
exploitenomah <iksploiting@gmail.com>
2023-03-10 15:44:46 +0100

    handle notifications close

--- components/Custom/Notification.tsx
+++ components/Custom/Notification.tsx
@@ -1,3 +1,39 @@
-export default function App() {
-  return <div className='absolute w-[280px] py-8'></div>
+import CloseIcon from '@assets/CloseIcon'
+import Button from '@components/HTML/Button'
+import { removeAppAlert } from '@store/notifications/slice'
+import { useMemo } from 'react'
+import { useDispatch } from 'react-redux'
+import Alert from '../../types/Alert'
+
+export default function NotificationComponent({ message, variant, id }: Alert) {
+  const dispatch = useDispatch()
+  const _variant = useMemo(() => {
+    switch (variant) {
+      case 'error':
+        return 'error'
+      case 'primary':
+        return 'primary'
+      case 'warning':
+        return 'warning'
+      case 'success':
+        return 'success'
+      default:
+        return 'primary'
+    }
+  }, [variant])
+
+  return (
+    <div
+      role='alert'
+      className={`relative px-6 py-4 text-center ${_variant} left-0 z-[1000000] text-sm my-2 mr-3`}
+    >
+      <Button
+        onClick={() => dispatch(removeAppAlert(id))}
+        className='absolute top-[6px] right-[6px] p-0'
+      >
+        <CloseIcon />
+      </Button>
+      <div>{message}</div>
+    </div>
+  )
 }

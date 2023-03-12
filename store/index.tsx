585e5bce72b2fd572d7be0ba76a9040c8b2563ed
exploitenomah <iksploiting@gmail.com>
2023-03-12 10:33:31 +0100

    create user reducer

--- store/index.tsx
+++ store/index.tsx
@@ -2,9 +2,10 @@ import { configureStore } from '@reduxjs/toolkit'
 import ui from './ui/slice'
 import auth from './auth/slice'
 import notification from './notifications/slice'
+import user from './user/slice'
 
 const store = configureStore({
-  reducer: { ui, auth, notification },
+  reducer: { ui, auth, notification, user },
 })
 const state = store.getState()
 export type Store = typeof state

3a2117574829a1a8607acf48b73ccb96a118111e
exploitenomah <iksploiting@gmail.com>
2023-03-13 15:17:39 +0100

    add friends and conversations reducers to state

--- store/index.tsx
+++ store/index.tsx
@@ -3,9 +3,11 @@ import ui from './ui/slice'
 import auth from './auth/slice'
 import notification from './notifications/slice'
 import user from './user/slice'
+import friends from './friends/slice'
+import conversations from './conversations/slice'
 
 const store = configureStore({
-  reducer: { ui, auth, notification, user },
+  reducer: { ui, auth, notification, user, friends, conversations },
 })
 const state = store.getState()
 export type Store = typeof state

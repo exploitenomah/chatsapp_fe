f0d8b200b6fd79daf592a21af9ed154475be9bb6
exploitenomah <iksploiting@gmail.com>
2023-03-14 06:01:03 +0100

    create useEmitter hook  for emitting socket events

---
+++ hooks/useEmitters.tsx
@@ -0,0 +1,16 @@
+import { useMemo } from 'react'
+import { Socket } from 'socket.io-client'
+
+export default function useEmitter(socket: Socket, events: string[]) {
+  const emitters = useMemo(() => {
+    return events.reduce((acc, event) => {
+      return {
+        ...acc,
+        [event]: (data: any) => {
+          socket.emit(event, data)
+        },
+      }
+    }, {})
+  }, [events, socket])
+  return emitters as {[x: string]: (data: any) => void}
+}

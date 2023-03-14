22cddde226ad5ef51e4c9a9dde93918c6402e6f8
exploitenomah <iksploiting@gmail.com>
2023-03-14 06:06:13 +0100

    create inactive left panel component

---
+++ components/Custom/App/InactiveLeftPanel.tsx
@@ -0,0 +1,22 @@
+import Button from '@components/HTML/Button'
+import { useDispatch } from 'react-redux'
+import { toggleShowFriendsDrawer } from '@store/ui/slice'
+
+export default function InactiveLeftPanel() {
+  const dispatch = useDispatch()
+  return (
+    <>
+      <div className='bg-primary-medium h-full flex justify-center items-center text-center border-b-4 border-b-accent-dark'>
+        <div>
+          <div className='mb-3'>No Conversations</div>
+          <Button
+            onClick={() => dispatch(toggleShowFriendsDrawer())}
+            className='bg-primary-light hover:brightness-100 hover:scale-105 transition-all duration-500 ease'
+          >
+            Create
+          </Button>
+        </div>
+      </div>
+    </>
+  )
+}

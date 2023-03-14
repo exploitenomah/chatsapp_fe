83d1ee67a37d2ac37ae7a426e74c4094643649c0
exploitenomah <iksploiting@gmail.com>
2023-03-14 06:03:29 +0100

    remove unused variable

--- store/auth/slice.tsx
+++ store/auth/slice.tsx
@@ -1,5 +1,4 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { User } from '../../types/User'
 import initialState from './initialState'
 
 export const authSlice = createSlice({

0935951d12a2a1703d1827c6343909b0d2d00ec6
exploitenomah <iksploiting@gmail.com>
2023-03-14 06:01:29 +0100

    create useHandler hook for handling socket events

---
+++ hooks/useHandlers.tsx
@@ -0,0 +1,24 @@
+import { ActionCreatorWithPayload, CaseReducerActions } from '@reduxjs/toolkit'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { Socket } from 'socket.io-client'
+
+export default function useHandlers(
+  socket: Socket,
+  events: string[],
+  actions: CaseReducerActions<
+    {
+      [x: string]: ActionCreatorWithPayload<unknown, string>
+    },
+    string
+  >,
+) {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    events.map((event) => {
+      return socket.on(event, (data) => {
+        dispatch(actions[event](data))
+      })
+    })
+  }, [actions, dispatch, events, socket])
+}

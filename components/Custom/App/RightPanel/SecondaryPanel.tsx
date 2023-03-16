7435f3364e74cad0b344420b305a5aa8af1d132b
exploitenomah <iksploiting@gmail.com>
2023-03-16 20:17:46 +0100

    create secondary right panel component

---
+++ components/Custom/App/RightPanel/SecondaryPanel.tsx
@@ -0,0 +1,21 @@
+import { ReactNode, useMemo } from 'react'
+
+export default function SecondaryPanel({
+  children,
+  show,
+}: {
+  children: ReactNode | ReactNode[]
+  show: boolean
+}) {
+  return (
+    <>
+      <div
+        className={`relative h-full bg-primary-darkest transition-all duration-300 overflow-hidden ${
+          show ? ' w-2/5 ' : ' w-0 '
+        }`}
+      >
+        {children}
+      </div>
+    </>
+  )
+}

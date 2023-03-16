737fadb868533f119d38267e0ff0e76523b103be
exploitenomah <iksploiting@gmail.com>
2023-03-16 06:42:04 +0100

    store friends suggestions in state

--- store/friends/slice.tsx
+++ store/friends/slice.tsx
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { User } from '@store/user/initialState'
 import { makeUniqueArrOfObjectsWithIdKey } from '@utils/index'
 import initialState, { Friend } from './initialState'
 
@@ -7,16 +8,26 @@ const friendsSlice = createSlice({
   initialState,
   reducers: {
     updateFriends: (state, action: PayloadAction<Friend[]>) => {
-      const allFriends = (state.friends = makeUniqueArrOfObjectsWithIdKey([
+      const allFriends = makeUniqueArrOfObjectsWithIdKey([
         ...state.friends,
         ...action.payload,
-      ]) as Friend[])
+      ]) as Friend[]
       state.friends = allFriends
       if (state.hasFetchedFriends === false) state.hasFetchedFriends = true
-    }
+    },
+    getSuggestions: (state, action: PayloadAction<User[]>) => {
+      state.suggestions = makeUniqueArrOfObjectsWithIdKey([
+        ...action.payload,
+        ...state.suggestions,
+      ])
+      state.suggestionsPage = state.suggestionsPage + 1
+      state.hasFetchedAllSuggestions = action.payload.length < state.limit
+    },
   },
 })
 
 export const { updateFriends } = friendsSlice.actions
+
+export const friendsActions = friendsSlice.actions
 
 export default friendsSlice.reducer

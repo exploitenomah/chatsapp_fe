2d5598201324ab420b30797e02041864808acfa4
exploitenomah <iksploiting@gmail.com>
2023-03-16 06:42:52 +0100

    create friend item component

---
+++ components/Custom/Friends/FriendItem.tsx
@@ -0,0 +1,30 @@
+import { User } from '@store/user/initialState'
+import { useState } from 'react'
+import Avatar from '../Avatar'
+
+export default function FriendItem({ user }: { user: User }) {
+  const [active, setActive] = useState(false)
+
+  return (
+    <div
+      onClick={() => setActive((prev) => !prev)}
+      className={`w-full h-[72px] flex items-center ${
+        active
+          ? 'bg-secondary-darkest'
+          : 'bg-primary-default hover:bg-secondary-default '
+      }`}
+    >
+      <div className='cursor-pointer flex items-center pr-[6px] w-full'>
+        <div className='px-[15px] flex justify-center items-center shrink-0'>
+          <Avatar width={49} height={49} />
+        </div>
+        <div className='h-[72px] basis-auto flex grow flex-col justify-center items-start border-t border-t-contrast-secondary/20'>
+          <div className='text-contrast-strong text-base'>{`${user.nickName}`}</div>
+          <div className='text-contrast-secondary text-sm font-normal whitespace-nowrap flex relative w-full h-[20px]'>
+            {`${user.firstName} ${user.lastName}`}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}

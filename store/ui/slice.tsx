320fbe9f6355fc9a45c940ff8074653d99f5d62d
exploitenomah <iksploiting@gmail.com>
2023-03-16 20:10:11 +0100

    add action handlers for user in preview

--- store/ui/slice.tsx
+++ store/ui/slice.tsx
@@ -1,9 +1,10 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import initialState from './initialState'
+import { User } from '@store/user/initialState'
+import initialState, { UI } from './initialState'
 
 export const uiSlice = createSlice({
   name: 'ui',
-  initialState,
+  initialState: initialState as UI,
   reducers: {
     toggleAppLoading: (state, action: PayloadAction<boolean>) => {
       state.appLoading = action.payload
@@ -29,6 +30,12 @@ export const uiSlice = createSlice({
     updateLoading: (state, action: PayloadAction<boolean>) => {
       state.loading = action.payload
     },
+    updateUserInPreview: (state, action: PayloadAction<User>) => {
+      state.userInPreview = action.payload
+    },
+    removeUserInPreview: (state) => {
+      state.userInPreview = null
+    },
   },
 })
 
@@ -40,6 +47,8 @@ export const {
   toggleShowSignup,
   toggleAppLoading,
   updateLoading,
+  updateUserInPreview,
+  removeUserInPreview,
 } = uiSlice.actions
 
 export default uiSlice.reducer

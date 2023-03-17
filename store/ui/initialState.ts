9e787ebe954dbe4264e3a2f4ebd1d22c041cca72
exploitenomah <iksploiting@gmail.com>
2023-03-17 06:56:49 +0100

    update ui initial state

--- store/ui/initialState.ts
+++ store/ui/initialState.ts
@@ -1,16 +1,23 @@
 import { User } from '@store/user/initialState'
 
-const initialState = {
+const initialState: UI = {
   showFriendsDrawer: false,
   showFriendsSuggestionDrawer: false,
   showLogin: false,
   showSignup: false,
   appLoading: true,
   loading: false,
+  userInPreview: null,
 }
 
-export type UI = typeof initialState & {
-  userInPreview: null | (User & { isFriend?: boolean })
+export type UI = {
+  showFriendsDrawer: boolean
+  showFriendsSuggestionDrawer: boolean
+  showLogin: boolean
+  showSignup: boolean
+  appLoading: boolean
+  loading: boolean
+  userInPreview: null | (User & { isFriend?: boolean; isPending?: boolean })
 }
 
 export default initialState

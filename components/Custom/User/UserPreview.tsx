4481041aed50cc928099337475d7eeadcbbac8d5
exploitenomah <iksploiting@gmail.com>
2023-03-17 06:51:52 +0100

    update pending state if user in preview has been sent a friend request

--- components/Custom/User/UserPreview.tsx
+++ components/Custom/User/UserPreview.tsx
@@ -1,8 +1,10 @@
 import CloseIcon from '@assets/CloseIcon'
 import Button from '@components/HTML/Button'
+import { FriendsState } from '@store/friends/initialState'
 import { Store } from '@store/index'
 import { UI } from '@store/ui/initialState'
-import { removeUserInPreview } from '@store/ui/slice'
+import { removeUserInPreview, updateUserInPreview } from '@store/ui/slice'
+import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { headerClasses } from '../App/AppHeader'
 import SecondaryPanel from '../App/RightPanel/SecondaryPanel'
@@ -10,7 +12,22 @@ import Profile from './Profile'
 
 export default function UserPreview() {
   const { userInPreview } = useSelector<Store, UI>((store) => store.ui)
+  const { pendingFriends } = useSelector<Store, FriendsState>(
+    (store) => store.friends,
+  )
   const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (
+      userInPreview &&
+      pendingFriends.find(
+        (pendingFriend) => userInPreview._id === pendingFriend.recipient,
+      ) &&
+      !userInPreview.isPending
+    ) {
+      dispatch(updateUserInPreview({ ...userInPreview, isPending: true }))
+    }
+  }, [dispatch, pendingFriends, userInPreview])
 
   return (
     <SecondaryPanel show={Boolean(userInPreview)}>

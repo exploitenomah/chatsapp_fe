3f747752d3ed47565abf62a74a53bf3f14e0e44b
exploitenomah <iksploiting@gmail.com>
2023-03-18 14:46:43 +0100

    create more actions

--- store/friends/slice.tsx
+++ store/friends/slice.tsx
@@ -1,40 +1,83 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { User } from '@store/user/initialState'
-import { makeUniqueArrOfObjectsWithIdKey } from '@utils/index'
+import { makeUniqueArrOfObjectsWith_IdKey } from '@utils/index'
 import initialState, { Friend } from './initialState'
 
 const friendsSlice = createSlice({
   name: 'friends',
   initialState,
   reducers: {
+    updateUser: (state, action: PayloadAction<User | null>) => {
+      state.user = action.payload
+    },
     updateFriends: (state, action: PayloadAction<Friend[]>) => {
-      const allFriends = makeUniqueArrOfObjectsWithIdKey([
+      const allFriends = makeUniqueArrOfObjectsWith_IdKey([
         ...state.friends,
         ...action.payload,
       ])
       state.friends = allFriends
-      if (state.hasFetchedFriends === false) state.hasFetchedFriends = true
     },
     getSuggestions: (state, action: PayloadAction<User[]>) => {
-      state.suggestions = makeUniqueArrOfObjectsWithIdKey([
+      state.suggestions = makeUniqueArrOfObjectsWith_IdKey([
         ...action.payload,
         ...state.suggestions,
       ])
       state.suggestionsPage = state.suggestionsPage + 1
       state.hasFetchedAllSuggestions = action.payload.length < state.limit
+      if (state.hasFetchedInitialSuggestions === false)
+        state.hasFetchedInitialSuggestions = true
     },
     request: (state, action: PayloadAction<Friend>) => {
-      if(!action.payload.is_valid){
-        state.pendingFriends = makeUniqueArrOfObjectsWithIdKey([
-        ...state.pendingFriends,
-        action.payload,
-      ])
+      const payload = action.payload
+      if (!payload.isValid) {
+        if (payload.requester === state.user?._id) {
+          state.pendingFriends = makeUniqueArrOfObjectsWith_IdKey([
+            ...state.pendingFriends,
+            payload,
+          ])
+        } else if (payload.recipient === state.user?._id) {
+          state.friendRequests = makeUniqueArrOfObjectsWith_IdKey([
+            ...state.friendRequests,
+            payload,
+          ])
+        }
       }
+    },
+    remove: (state, action: PayloadAction<Friend>) => {
+      const payload = action.payload
+      const filterOutPayload = (req: Friend) => req._id !== payload._id
+      state.friendRequests = state.friendRequests.filter(filterOutPayload)
+      state.pendingFriends = state.pendingFriends.filter(filterOutPayload)
+      state.friends = state.friends.filter(filterOutPayload)
+    },
+    getMany: (state, action: PayloadAction<Friend[]>) => {
+      const payload = action.payload
+      const usersFriends = payload.filter((item) => item.isValid === true)
+      state.friends = makeUniqueArrOfObjectsWith_IdKey([
+        ...state.friends,
+        ...usersFriends,
+      ])
+      const friendRequests = payload.filter(
+        (item) => item.isValid === false && item.recipient === state.user?._id,
+      )
+      state.friendRequests = makeUniqueArrOfObjectsWith_IdKey([
+        ...state.friendRequests,
+        ...friendRequests,
+      ])
+      const pendingFriends = payload.filter(
+        (item) => item.isValid === false && item.requester === state.user?._id,
+      )
+      state.pendingFriends = makeUniqueArrOfObjectsWith_IdKey([
+        ...state.pendingFriends,
+        ...pendingFriends,
+      ])
+      state.friendsPage = state.friendsPage + 1
+      state.hasFetchedAllFriends = payload.length < state.limit
     },
   },
 })
 
-export const { updateFriends } = friendsSlice.actions
+export const { updateFriends, updateUser } = friendsSlice.actions
 
 export const friendsActions = friendsSlice.actions
 

1a416f3493e493f8f11503c3bceb2263781ade8c
exploitenomah <iksploiting@gmail.com>
2023-03-18 14:54:12 +0100

    create user hooks

---
+++ hooks/user/useLogout.tsx
@@ -0,0 +1,17 @@
+import { updateUser } from '@store/friends/slice'
+import { logout } from '@store/auth/slice'
+import { clearUser } from '@store/user/slice'
+import { useCallback } from 'react'
+import { useDispatch } from 'react-redux'
+
+export default function useLogout() {
+  const dispatch = useDispatch()
+  const logUserOut = useCallback(() => {
+    dispatch(updateUser(null))
+    dispatch(logout())
+    dispatch(clearUser())
+    localStorage.removeItem('chatsapp_token')
+  }, [dispatch])
+
+  return logUserOut
+}

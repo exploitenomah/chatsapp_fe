8f42efe34e94dbfbdae5236ff0fb15146f5c7d4b
exploitenomah <iksploiting@gmail.com>
2023-03-22 07:27:45 +0100

    show friend in preview when clicked, add message button

--- components/Custom/Friends/FriendsList.tsx
+++ components/Custom/Friends/FriendsList.tsx
@@ -1,14 +1,17 @@
+import Button from '@components/HTML/Button'
 import { Friend, FriendsState } from '@store/friends/initialState'
 import { Store } from '@store/index'
 import { UI } from '@store/ui/initialState'
+import { updateUserInPreview } from '@store/ui/slice'
 import { User } from '@store/user/initialState'
 import { useMemo } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Avatar from '../Avatar'
 
 const FriendItem = ({ friendItem }: { friendItem: Friend }) => {
   const authenticatedUser = useSelector<Store, User>((store) => store.user)
   const { userInPreview } = useSelector<Store, UI>((store) => store.ui)
+  const dispatch = useDispatch()
   const friend = useMemo(() => {
     if (friendItem.requester._id === authenticatedUser._id)
       return friendItem.recipient
@@ -19,21 +22,27 @@ const FriendItem = ({ friendItem }: { friendItem: Friend }) => {
 
   return (
     <div
-      className={`w-full h-[72px] flex items-center ${
+      onClick={() => dispatch(updateUserInPreview(friend))}
+      className={`w-full flex items-center ${
         userInPreview?._id === friend._id
-          ? 'bg-secondary-darkest'
-          : 'bg-primary-default hover:bg-secondary-default '
+          ? 'bg-secondary-darkest/25'
+          : 'bg-primary-default'
       }`}
     >
       <div className='cursor-pointer flex items-center pr-[6px] w-full'>
         <div className='px-[15px] flex justify-center items-center shrink-0'>
           <Avatar width={49} height={49} />
         </div>
-        <div className='h-[72px] basis-auto flex grow flex-col justify-center items-start border-t border-t-contrast-secondary/20'>
-          <div className='text-contrast-strong text-base'>{`${friend.nickName}`}</div>
-          <div className='text-contrast-secondary text-sm font-normal whitespace-nowrap flex relative w-full h-[20px]'>
-            {`${friend.firstName} ${friend.lastName}`}
+        <div className='basis-auto flex grow justify-between items-center py-3 border-t border-t-contrast-secondary/20'>
+          <div>
+            <div className='text-contrast-strong text-base'>{`${friend.nickName}`}</div>
+            <div className='text-contrast-secondary text-sm font-normal whitespace-nowrap flex relative w-full h-[20px]'>
+              {`${friend.firstName} ${friend.lastName}`}
+            </div>
           </div>
+          <Button className='bg-accent-default py-2 px-3 text-contrast-strong'>
+            Message
+          </Button>
         </div>
       </div>
     </div>

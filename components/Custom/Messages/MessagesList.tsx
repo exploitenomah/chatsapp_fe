571c47ece5fb34501dd4c01d0eafd0489ad717c9
exploitenomah <iksploiting@gmail.com>
2023-03-23 16:41:53 +0100

    render messages

--- components/Custom/Messages/MessagesList.tsx
+++ components/Custom/Messages/MessagesList.tsx
@@ -1,172 +1,37 @@
-import CurrentUserMsg from './CurrentUser'
-import OtherUserMsg from './OtherUser'
+import { Store } from '@store/index'
+import { UI } from '@store/ui/initialState'
+import { useSelector } from 'react-redux'
+import { useMemo } from 'react'
+import MessageComponent, { MessageWrapper } from './Message'
 
 export default function MessagesList() {
+  const { activeConversation } = useSelector<Store, UI>((store) => store.ui)
+
+  const messages = useMemo(() => {
+    if (!activeConversation || !Array.isArray(activeConversation.messages))
+      return []
+    else {
+      return activeConversation.messages
+        .map((msg) => ({
+          ...msg,
+          createdAt: new Date(msg.createdAt),
+        }))
+        .sort((a, b) => a.createdAt.getTime() - b.createdAt.getTime())
+      // .map(msg=> ({...msg, createdAt: msg.createdAt.toDateString()}))
+    }
+  }, [activeConversation])
+
   return (
     <div className='w-full text-sm py-6'>
       <div className='flex flex-col'>
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
-        <CurrentUserMsg />
-        <OtherUserMsg />
+        {messages.map((message, idx, arr) => (
+          <MessageWrapper
+            isConcurrentSender={message.sender === arr[idx + 1]?.sender}
+            key={message._id}
+          >
+            <MessageComponent message={message} />
+          </MessageWrapper>
+        ))}
       </div>
     </div>
   )
--- components/Custom/Messages/OtherUser.tsx
+++ components/Custom/Messages/OtherUser.tsx
@@ -1,18 +1,19 @@
 import MessageTailIn from '@assets/MessageTailIn'
+import { Message } from '@store/messages/initialState'
 
-export default function OtherUserMsg() {
+export default function OtherUserMsg({ message }: { message: Message }) {
   return (
-    <div className='px-16 flex justify-start mb-3'>
-      <div className='pt-1.5 pb-2.5 px-2 rounded-bl-lg rounded-br-lg rounded-tr-lg w-fit max-w-[75%] relative font-sm leading-5 text-contrast-strong bg-secondary-default'>
+    <div className='px-16 flex justify-start'>
+      <div className='pt-1.5 pb-2.5 px-2 rounded-bl-lg rounded-br-lg rounded-tr-lg w-fit max-w-[75%] relative font-sm leading-5 text-contrast-strong bg-secondary-default flex'>
         <div className='absolute top-0 -left-2 text-secondary-default'>
           <MessageTailIn />
         </div>
         <div className='whitespace-pre-wrap'>
-          <span>Omo, I don&apos;t even know</span>
+          <span>{message.text}</span>
         </div>
-        <div>
-          <div className='float-right ml-1 -mb-1.5 whitespace-nowrap text-[11px] text-contrast-primary/70'>
-            02:19
+        <div className='self-end ml-1'>
+          <div className='self-end ml-1 -mb-1.5 whitespace-nowrap text-[11px] text-contrast-primary/70'>
+            {new Date(message.createdAt).toLocaleTimeString().slice(0, 5)}
           </div>
         </div>
       </div>

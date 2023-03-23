27a7881caae44131409549050c5cdc20ac0f51e4
exploitenomah <iksploiting@gmail.com>
2023-03-23 16:41:16 +0100

    set up sending messages

--- components/Custom/MessageInput/index.tsx
+++ components/Custom/MessageInput/index.tsx
@@ -1,11 +1,34 @@
 import { SendIcon, EmojiIcon, FileInputIcon } from '@assets/index'
 import Button from '@components/HTML/Button'
+import useSendMessage from '@hooks/messages/useSendMessage'
+import { Store } from '@store/index'
+import { Message } from '@store/messages/initialState'
+import { UI } from '@store/ui/initialState'
+import { User } from '@store/user/initialState'
 import { useCallback, useEffect, useRef, useState } from 'react'
+import { useSelector } from 'react-redux'
 import TextInput from './TextInput'
 
 export default function MessageInput() {
   const textInputRef = useRef<HTMLTextAreaElement>(null)
-  const [test, setTest] = useState('')
+  const [message, setMessage] = useState({
+    text: '',
+    attachments: [],
+  })
+
+  const user = useSelector<Store, User>((store) => store.user)
+  const { activeConversation } = useSelector<Store, UI>((store) => store.ui)
+
+  const handleSendMessage = useSendMessage()
+
+  const sendMessage = useCallback(() => {
+    if (message.text.trim().length === 0 && message.attachments.length === 0)
+      return setMessage((prev) => ({ ...prev, text: '' }))
+    if (activeConversation) {
+      handleSendMessage(activeConversation, { ...message, sender: user._id })
+       setMessage((prev) => ({ ...prev, text: '' }))
+    }
+  }, [activeConversation, handleSendMessage, message, user._id])
 
   return (
     <div className='py-1 pr-[17px] pl-[10px] flex items-end border-l border-l-contrast-secondary/20 bg-secondary-default'>
@@ -21,11 +44,16 @@ export default function MessageInput() {
         <div className='px-3 py-[9px] my-1 mx-2 rounded-lg w-full bg-secondary-darkest'>
           <TextInput
             textInputRef={textInputRef}
-            onChange={function (value): void {
-              setTest(value)
+            onChange={(value) => {
+              setMessage((prev) => ({ ...prev, text: value }))
+            }}
+            onKeyDown={(e) => {
+              if (e.shiftKey && e.key === 'Enter') return
+              e.key === 'Enter' &&
+                !e.shiftKey && [e.preventDefault(), sendMessage()]
             }}
             onFocus={(e) => console.log('focused', e.target.selectionStart)}
-            value={test}
+            value={message.text}
           />
         </div>
         <Button className='pt-1 pb-3.5 px-2.5 self-end text-contrast-secondary/90'>

07c5e03b9bfd1b8d1c1865f23c4ad835acd90522
exploitenomah <iksploiting@gmail.com>
2023-03-25 04:17:57 +0100

    render messages

--- components/Custom/Messages/MessagesList.tsx
+++ components/Custom/Messages/MessagesList.tsx
@@ -1,11 +1,14 @@
 import { Store } from '@store/index'
 import { UI } from '@store/ui/initialState'
 import { useSelector } from 'react-redux'
-import { useMemo } from 'react'
-import MessageComponent, { MessageWrapper } from './Message'
+import { useCallback, useMemo } from 'react'
+import MessageComponent, { MainMessageWrapper } from './Message'
+import { User } from '@store/user/initialState'
+import { Message } from '@store/messages/initialState'
 
 export default function MessagesList() {
   const { activeConversation } = useSelector<Store, UI>((store) => store.ui)
+  const authenticatedUser = useSelector<Store, User>((store) => store.user)
 
   const messages = useMemo(() => {
     if (!activeConversation || !Array.isArray(activeConversation.messages))
@@ -17,22 +20,45 @@ export default function MessagesList() {
           createdAt: new Date(msg.createdAt),
         }))
         .sort((a, b) => a.createdAt.getTime() - b.createdAt.getTime())
-      // .map(msg=> ({...msg, createdAt: msg.createdAt.toDateString()}))
     }
   }, [activeConversation])
 
+  const getIfShouldScrollMessageIntoView = useCallback(
+    (message: Message, idx: number, arr: Message[]) => {
+      return (
+        (idx === arr.length - 1 &&
+          (message.sender === authenticatedUser._id ||
+            arr.every((msg: { seen: boolean }) => msg.seen === true))) ||
+        (idx === 0 &&
+          arr.every((msg: { seen: boolean }) => msg.seen === false)) ||
+        (message.seen === false &&
+          message.sender !== authenticatedUser._id &&
+          (arr[idx - 1]?.seen === true ||
+            arr[idx - 1]?.sender === authenticatedUser._id))
+      )
+    },
+    [authenticatedUser._id],
+  )
+  if (messages.length === 0) return null
   return (
-    <div className='w-full text-sm py-6'>
-      <div className='flex flex-col'>
-        {messages.map((message, idx, arr) => (
-          <MessageWrapper
-            isConcurrentSender={message.sender === arr[idx + 1]?.sender}
-            key={message._id}
-          >
-            <MessageComponent message={message} />
-          </MessageWrapper>
-        ))}
-      </div>
-    </div>
+    <>
+      {messages.map((message, idx, arr) => (
+        <MainMessageWrapper
+          isConcurrentSender={
+            message.sender === arr[idx + 1]?.sender || idx === arr.length - 1
+          }
+          key={message._id}
+        >
+          <MessageComponent
+            message={message}
+            scrollMessageIntoView={getIfShouldScrollMessageIntoView(
+              message,
+              idx,
+              arr,
+            )}
+          />
+        </MainMessageWrapper>
+      ))}
+    </>
   )
 }

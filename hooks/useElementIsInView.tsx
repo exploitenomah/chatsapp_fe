bdcbb65996fc5fbb79050b94c055fb28dfda86eb
exploitenomah <iksploiting@gmail.com>
2023-03-26 21:03:28 +0100

    update callback function arguments

--- hooks/useElementIsInView.tsx
+++ hooks/useElementIsInView.tsx
@@ -2,14 +2,15 @@ import { useCallback } from 'react'
 
 export default function useElementIsInView() {
   const handleElementInView = useCallback(
-    (currentElement: HTMLElement | null, onElementInView: () => void) => {
+    (
+      currentElement: HTMLElement | null,
+      onElementInView: (elementIsInView: boolean) => void,
+    ) => {
       const elementCurrentParent = currentElement?.parentElement
       let elementIsInView =
         currentElement?.offsetTop! - elementCurrentParent?.scrollTop! <=
         elementCurrentParent?.clientHeight!
-      if (elementIsInView) {
-        onElementInView()
-      }
+      onElementInView(elementIsInView)
     },
     [],
   )

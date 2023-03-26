8fe4a4460bdba08ef0ffce0128aafcf9c47a29d6
exploitenomah <iksploiting@gmail.com>
2023-03-26 21:01:51 +0100

    refactor step 1

--- hooks/messages/useOnUpdateMessage.tsx
+++ hooks/messages/useOnUpdateMessage.tsx
@@ -3,12 +3,16 @@ import { Store } from '@store/index'
 import { UI } from '@store/ui/initialState'
 import { useDispatch, useSelector } from 'react-redux'
 import { updateActiveConversation } from '@store/ui/slice'
+import { ConversationsState } from '@store/conversations/initialState'
+import { updateSingleConversation } from '@store/conversations/slice'
 
 export default function useOnUpdateMessages() {
   const dispatch = useDispatch()
 
   const { activeConversation } = useSelector<Store, UI>((store) => store.ui)
-
+  const { conversations } = useSelector<Store, ConversationsState>(
+    (store) => store.conversations,
+  )
   const handleUpdateMessagesSeen = useCallback(
     (acknowledgement: { acknowledged: boolean; conversationId: string }) => {
       if (acknowledgement.conversationId === activeConversation?._id) {
@@ -22,8 +26,25 @@ export default function useOnUpdateMessages() {
           }),
         )
       }
+      const conversationInState = conversations.filter(
+        (conversation) => conversation._id === acknowledgement.conversationId,
+      )[0]
+      if (conversationInState) {
+        dispatch(
+          updateSingleConversation({
+            conversationId: acknowledgement.conversationId,
+            update: {
+              ...conversationInState,
+              messages: (conversationInState.messages || []).map((msg) => ({
+                ...msg,
+                seen: true,
+              })),
+            },
+          }),
+        )
+      }
     },
-    [activeConversation, dispatch],
+    [activeConversation, conversations, dispatch],
   )
   return handleUpdateMessagesSeen
 }

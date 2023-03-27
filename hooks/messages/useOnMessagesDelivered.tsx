58d012113c0596effa8f101f4c5b30e1d7acbcd4
exploitenomah <iksploiting@gmail.com>
2023-03-27 14:21:06 +0100

    create hook for messages delivered

---
+++ hooks/messages/useOnMessagesDelivered.tsx
@@ -0,0 +1,56 @@
+import { useCallback } from 'react'
+import { Store } from '@store/index'
+import { UI } from '@store/ui/initialState'
+import { useDispatch, useSelector } from 'react-redux'
+import { updateActiveConversation } from '@store/ui/slice'
+import { ConversationsState } from '@store/conversations/initialState'
+import { updateSingleConversation } from '@store/conversations/slice'
+
+export default function useOnUpdateMessagesDelivered() {
+  const dispatch = useDispatch()
+
+  const { activeConversation } = useSelector<Store, UI>((store) => store.ui)
+  const { conversations } = useSelector<Store, ConversationsState>(
+    (store) => store.conversations,
+  )
+  const handleUpdateMessagesDelivered = useCallback(
+    (acknowledgement: { acknowledged: boolean; conversationId: string }) => {
+      if (acknowledgement.conversationId === activeConversation?._id) {
+        dispatch(
+          updateActiveConversation({
+            ...activeConversation,
+            messages: (activeConversation?.messages || []).map((msg) => ({
+              ...msg,
+              delivered: true,
+            })),
+          }),
+        )
+      }
+      const conversationInState = conversations.filter(
+        (conversation) => conversation._id === acknowledgement.conversationId,
+      )[0]
+      if (conversationInState) {
+        dispatch(
+          updateSingleConversation({
+            conversationId: acknowledgement.conversationId,
+            update: {
+              ...conversationInState,
+              messages: (conversationInState.messages || []).map((msg) => ({
+                ...msg,
+                delivered: true,
+              })),
+              latestMessage: conversationInState.latestMessage
+                ? {
+                    ...conversationInState.latestMessage,
+                    delivered: true,
+                  }
+                : undefined,
+            },
+          }),
+        )
+      }
+    },
+    [activeConversation, conversations, dispatch],
+  )
+  return handleUpdateMessagesDelivered
+}

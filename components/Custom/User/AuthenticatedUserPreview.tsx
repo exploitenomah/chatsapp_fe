e3cbc82066229cfc6fe4bbd68021373c918a20b8
exploitenomah <iksploiting@gmail.com>
2023-04-09 05:24:19 +0100

    refactor

--- components/Custom/User/AuthenticatedUserPreview.tsx
+++ components/Custom/User/AuthenticatedUserPreview.tsx
@@ -1,33 +1,19 @@
-import CloseIcon from '@assets/CloseIcon'
 import LeftArrow from '@assets/LeftArrow'
 import PencilIcon from '@assets/PencilIcon'
 import SingleCheckIcon from '@assets/SingleCheckIcon'
 import Button from '@components/HTML/Button'
-import Input from '@components/HTML/Input'
-import useIsNickNameInvalid, {
-  isValidNickNameRegex,
-  useCheckIfNickNameIsValid,
-} from '@hooks/user/useIsNickNameInvalid'
+import { useCheckIfNickNameIsValid } from '@hooks/user/useIsNickNameInvalid'
 import useUser from '@sockets/useUser'
-import { Friend, FriendsState } from '@store/friends/initialState'
 import { Store } from '@store/index'
 import { UI } from '@store/ui/initialState'
-import {
-  removeUserInPreview,
-  toggleShowAuthenticatedUserProfile,
-  updateUserInPreview,
-} from '@store/ui/slice'
+import { toggleShowAuthenticatedUserProfile } from '@store/ui/slice'
 import { User } from '@store/user/initialState'
 import {
   ClipboardEventHandler,
-  EventHandler,
   FormEvent,
-  FormEventHandler,
   KeyboardEventHandler,
-  RefObject,
   useCallback,
   useEffect,
-  useMemo,
   useRef,
   useState,
 } from 'react'
@@ -46,19 +32,39 @@ function selectElmCnt(elm: any) {
   }
 }
 
+const Header = () => {
+  const dispatch = useDispatch()
+  return (
+    <>
+      <div className={`${headerClasses} flex items-center pb-4`}>
+        <header className='h-[80px] w-full flex justify-between items-end font-light text-contrast-tertiary text-xl bg-secondary-default px-3'>
+          <div className='flex items-center gap-x-1'>
+            <Button
+              className='p-0 w-12 font-light'
+              onClick={() =>
+                dispatch(toggleShowAuthenticatedUserProfile(false))
+              }
+            >
+              <LeftArrow />
+            </Button>
+            <span className=''>Profile</span>
+          </div>
+        </header>
+      </div>
+    </>
+  )
+}
 const FormTextDisplay = ({
   value,
   name,
   max,
   min,
-  alwaysReadOnly,
 }: {
   value: string
   onSubmit?: (value: string) => undefined | string
   max: number
   min: number
-  alwaysReadOnly?: boolean
-  name?: 'firstName' | 'lastName' | 'nickName' | 'about'
+  name: 'firstName' | 'lastName' | 'nickName' | 'about'
 }) => {
   const userSocket = useUser()
   const [isEditing, setIsEditing] = useState(false)
@@ -78,6 +84,7 @@ const FormTextDisplay = ({
     },
     [checkIfNickNameIsInValid, userSocket],
   )
+
   const handleSubmit = useCallback(
     (submitEvent: FormEvent | KeyboardEvent) => {
       submitEvent.preventDefault()
@@ -87,16 +94,11 @@ const FormTextDisplay = ({
         return setError('too short!')
       else if (updatedVal.length >= max) return setError('too long!')
       if (updatedVal === value) return setIsEditing(false)
-      if (!alwaysReadOnly && name) {
-        const error = handleUpdateInfo({ [name]: updatedVal })
-        if (error) {
-          setError(error)
-        } else {
-          setIsEditing(false)
-        }
-      }
+      const submitErr = handleUpdateInfo({ [name]: updatedVal })
+      if (submitErr) setError(submitErr)
+      else setIsEditing(false)
     },
-    [error.length, min, max, value, alwaysReadOnly, name, handleUpdateInfo],
+    [error.length, min, max, value, name, handleUpdateInfo],
   )
   const handlePaste: ClipboardEventHandler<HTMLDivElement> = useCallback(
     (e) => {
@@ -173,27 +175,75 @@ const FormTextDisplay = ({
           {value}
         </div>
       </>
-      {!alwaysReadOnly &&
-        (isEditing ? (
-          <Button
-            className='scale-125 p-0 text-contrast-primary/75 transition-color duration-300 pb-1'
-            type='submit'
-          >
-            <SingleCheckIcon />
-          </Button>
-        ) : (
-          <Button
-            className='p-0'
-            onClick={(e) => {
-              e.preventDefault()
-              setIsEditing((prev) => !prev)
-            }}
-            type='button'
-          >
-            <PencilIcon />
-          </Button>
-        ))}
+
+      <Button
+        className={`${
+          isEditing ? '' : 'hidden'
+        } scale-125 p-0 text-contrast-primary/75 transition-color duration-300 pb-1`}
+        type='submit'
+      >
+        <SingleCheckIcon />
+      </Button>
+      <Button
+        className={`${isEditing ? 'hidden' : ''} p-0`}
+        onClick={(e) => {
+          e.preventDefault()
+          setIsEditing((prev) => !prev)
+        }}
+        type='button'
+      >
+        <PencilIcon />
+      </Button>
     </form>
+  )
+}
+
+const NickNameFormTextDisplay = ({ nickName }: { nickName: string }) => {
+  return (
+    <>
+      <div className='w-full'>
+        <h3 className='text-sm text-accent-darkest'>Nick name </h3>
+        <FormTextDisplay max={50} min={3} name={'nickName'} value={nickName} />
+      </div>
+    </>
+  )
+}
+
+const FirstNameFormTextDisplay = ({ firstName }: { firstName: string }) => {
+  return (
+    <>
+      <div className='w-full'>
+        <h3 className='text-sm text-accent-darkest'>First name</h3>
+        <FormTextDisplay
+          max={250}
+          min={3}
+          name={'firstName'}
+          value={firstName}
+        />
+      </div>
+    </>
+  )
+}
+
+const LastNameFormTextDisplay = ({ lastName }: { lastName: string }) => {
+  return (
+    <>
+      <div className='w-full'>
+        <h3 className='text-sm text-accent-darkest'>Last name </h3>
+        <FormTextDisplay max={250} min={3} name={'lastName'} value={lastName} />
+      </div>
+    </>
+  )
+}
+
+const AboutFormTextDisplay = ({ about }: { about: string }) => {
+  return (
+    <>
+      <div className='w-full'>
+        <h3 className='text-sm text-accent-darkest'>About </h3>
+        <FormTextDisplay max={250} min={10} name={'about'} value={about} />
+      </div>
+    </>
   )
 }
 
@@ -209,83 +259,28 @@ export default function AuthenticatedUserPreview() {
   const { showAuthenticatedUserProfile } = useSelector<Store, UI>(
     (store) => store.ui,
   )
-  const dispatch = useDispatch()
 
   return (
     <LeftDrawer zIndex='z-[400]' show={showAuthenticatedUserProfile}>
       <div>
         <div className='h-full w-full'>
-          <div className={`${headerClasses} flex items-center pb-4`}>
-            <header className='h-[80px] w-full flex justify-between items-end font-light text-contrast-tertiary text-xl bg-secondary-default px-3'>
-              <div className='flex items-center gap-x-1'>
-                <Button
-                  className='p-0 w-12 font-light'
-                  onClick={() =>
-                    dispatch(toggleShowAuthenticatedUserProfile(false))
-                  }
-                >
-                  <LeftArrow />
-                </Button>
-                <span className=''>Profile</span>
-              </div>
-            </header>
-          </div>
+          <Header />
           {showAuthenticatedUserProfile && (
             <div className='bg-primary-default py-7'>
               <div className='flex justify-center items-center mb-4'>
                 <Avatar width={200} height={200} />
               </div>
               <div className='flex flex-col justify-start items-start gap-y-3 w-[90%] mx-auto my-12'>
-                <div className='w-full'>
-                  <h3 className='text-sm text-accent-darkest'>Nick name </h3>
-                  <FormTextDisplay
-                    max={50}
-                    min={3}
-                    name={'nickName'}
-                    value={authenticatedUser.nickName}
-                  />
-                </div>
-                <div className='w-full'>
-                  <h3 className='text-sm text-accent-darkest'>First name </h3>
-                  <FormTextDisplay
-                    max={250}
-                    min={3}
-                    name={'firstName'}
-                    value={authenticatedUser.firstName}
-                  />
-                </div>
-                <div className='w-full'>
-                  <h3 className='text-sm text-accent-darkest'>Last name </h3>
-                  <FormTextDisplay
-                    max={250}
-                    min={3}
-                    name={'lastName'}
-                    value={authenticatedUser.lastName}
-                  />
-                </div>
-                <div className='w-full'>
-                  <h3 className='text-sm text-accent-darkest'>About </h3>
-                  <FormTextDisplay
-                    max={250}
-                    min={10}
-                    name={'about'}
-                    value={authenticatedUser.about}
-                  />
-                </div>
-                {/* <div className='w-full'>
-                  <h3 className='text-sm text-accent-darkest flex justify-between'>
-                    Email
-                  </h3>
-                  <FormTextDisplay
-                    max={50}
-                    min={3}
-                    value={authenticatedUser.email}
-                    alwaysReadOnly={true}
-                  />
-                  <Button className='p-0 block text-xs cursor-pointer hover:text-accent-dark text-contrast-secondary/70 ml-auto -mt-6'>
-                    change
-                  </Button>
-                </div> */}
+                <NickNameFormTextDisplay
+                  nickName={authenticatedUser.nickName}
+                />
+                <FirstNameFormTextDisplay
+                  firstName={authenticatedUser.firstName}
+                />
+                <LastNameFormTextDisplay
+                  lastName={authenticatedUser.lastName}
+                />
+                <AboutFormTextDisplay about={authenticatedUser.about} />
               </div>
             </div>
           )}

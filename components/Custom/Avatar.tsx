9692be000be835b0c3b6a1d7311e8ab32e84c205
exploitenomah <iksploiting@gmail.com>
2023-04-24 07:29:16 +0100

    refactor

--- components/Custom/Avatar.tsx
+++ components/Custom/Avatar.tsx
@@ -1,23 +1,41 @@
 import UserIcon from '@assets/UserIcon'
 import Image, { ImageProps } from 'next/image'
-
+import { useState } from 'react'
 export default function Avatar({
   width,
   height,
   loading,
   src,
   alt,
+  handleError,
+  handleLoad,
 }: ImageProps & {
   src?: ImageProps['src']
+  handleError?: () => void
+  handleLoad?: () => void
 }) {
-  if (!src)
+  const [srcLoaded, setSrcLoaded] = useState(true)
+  if (!src || !srcLoaded)
     return (
       <div
-        className={`flex justify-center items-center`}
+        className={`flex justify-center items-center rounded-full`}
         style={{ width: `${width}px`, height: `${height}px` }}
       >
         <UserIcon className='' />
       </div>
     )
-  return <Image {...{ src, width, height, loading, alt }} />
+  return (
+    <Image
+      onLoad={() => {
+        setSrcLoaded(true)
+        handleLoad && handleLoad()
+      }}
+      onError={() => {
+        setSrcLoaded(false)
+        handleError && handleError()
+      }}
+      className='text-center rounded-full '
+      {...{ src, width, height, loading, alt }}
+    />
+  )
 }

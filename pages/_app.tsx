2094d231f894f6d97c67040362920cfdf8f51d9d
exploitenomah <iksploiting@gmail.com>
2023-04-24 07:30:23 +0100

    add portal for image preview

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,6 +19,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <div>
         <Component {...pageProps} />
       </div>
+      <div id='image-preview-container'></div>
     </Provider>
   )
 }

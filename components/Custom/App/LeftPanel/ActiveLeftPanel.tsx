9c45ad5447b5e5bb64f75f599621e2b12fc54dab
exploitenomah <iksploiting@gmail.com>
2023-05-19 20:42:03 +0100

    add search bar for chats

--- components/Custom/App/LeftPanel/ActiveLeftPanel.tsx
+++ components/Custom/App/LeftPanel/ActiveLeftPanel.tsx
@@ -1,17 +1,35 @@
 import ConversationsList from '@components/Custom/Conversations/ConversationsList'
 import SearchBar from '../../SearchBar'
+import { ConversationsState } from '@store/conversations/initialState'
+import { Store } from '@store/index'
+import { useDispatch, useSelector } from 'react-redux'
+import { updateSearchText } from '@store/conversations/slice'
+
+const ConversationsSearch = () => {
+  const { searchText } = useSelector<Store, ConversationsState>(
+    (store) => store.conversations,
+  )
+  const dispatch = useDispatch()
+  return (
+    <SearchBar
+      inputProps={{
+        placeholder: 'Search',
+        value: searchText,
+        onChange: (e) => dispatch(updateSearchText(e.target.value)),
+      }}
+    />
+  )
+}
 
 export default function ActiveLeftPanel() {
   return (
     <>
       <div className='sticky top-0 w-full z-10'>
-        {/* <div className='w-11/12 mx-auto py-2'>
-          <SearchBar
-            inputProps={{ placeholder: 'Search or start a new chat top-[52px]' }}
-          />
-        </div> */}
+        <div className='w-11/12 mx-auto py-2'>
+          <ConversationsSearch />
+        </div>
       </div>
-      <div className='absolute bottom-0 w-full overflow-auto top-2'>
+      <div className='absolute bottom-0 w-full overflow-auto top-[52px]'>
         <ConversationsList />
       </div>
     </>

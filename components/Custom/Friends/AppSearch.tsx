e57d606e86460a778a96583939b0cca0ff60e6e4
exploitenomah <iksploiting@gmail.com>
2023-05-26 20:30:50 +0100

    refactor

--- components/Custom/Friends/AppSearch.tsx
+++ components/Custom/Friends/AppSearch.tsx
@@ -6,36 +6,41 @@ import { useMemo } from 'react'
 import { User } from '@store/user/initialState'
 import { isMatchingStrStart } from '@utils/index'
 
-export default function AppSearch() {
+const useSearchFriends = () => {
+  const authenticatedUser = useSelector<Store, User>((store) => store.user)
+  const { friends } = useSelector<Store, FriendsState>((store) => store.friends)
   const { searchText, searchResults } = useSelector<Store, SearchState>(
     (store) => store.search,
   )
-  const { friends } = useSelector<Store, FriendsState>((store) => store.friends)
-  const authenticatedUser = useSelector<Store, User>((store) => store.user)
-  const searchedItems = useMemo(() => {
+  const filteredFriends = useMemo(() => {
     if (searchText.length === 0) return friends
-    const friendsSearched = friends
-      .filter((friend) => {
-        const otherUser =
-          friend.requester._id !== authenticatedUser._id
-            ? friend.requester
-            : friend.recipient
+    return friends.filter((friend) => {
+      const otherUser =
+        friend.requester._id !== authenticatedUser._id
+          ? friend.requester
+          : friend.recipient
 
-        return (
-          isMatchingStrStart(otherUser.firstName, searchText) ||
-          isMatchingStrStart(otherUser.lastName, searchText) ||
-          isMatchingStrStart(otherUser.nickName, searchText)
-        )
-      })
-      .map((friend) => {
-        const otherUser =
-          friend.requester._id === authenticatedUser._id
-            ? friend.recipient
-            : friend.requester
-        return otherUser
-      })
-    return [...friendsSearched, ...searchResults]
-  }, [authenticatedUser._id, friends, searchResults, searchText])
+      return (
+        isMatchingStrStart(otherUser.firstName, searchText) ||
+        isMatchingStrStart(otherUser.lastName, searchText) ||
+        isMatchingStrStart(otherUser.nickName, searchText)
+      )
+    })
+  }, [authenticatedUser._id, friends, searchText])
+  return useMemo(() => {
+    let friendsAsUsers = filteredFriends.map((friend) => {
+      const otherUser =
+        friend.requester._id === authenticatedUser._id
+          ? friend.recipient
+          : friend.requester
+      return otherUser
+    })
+    return [...friendsAsUsers, ...searchResults]
+  }, [authenticatedUser._id, filteredFriends, searchResults])
+}
+
+export default function AppSearch() {
+  const searchedItems = useSearchFriends()
 
   return (
     <>

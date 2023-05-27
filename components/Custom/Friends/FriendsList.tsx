2a1d0a15c51572480af9a3b30213868a214e2e8e
exploitenomah <iksploiting@gmail.com>
2023-05-27 03:02:56 +0100

    export friend item

--- components/Custom/Friends/FriendsList.tsx
+++ components/Custom/Friends/FriendsList.tsx
@@ -13,36 +13,27 @@ import { useDispatch, useSelector } from 'react-redux'
 import Avatar from '../Avatar'
 import { MessageButton } from '../User/Profile'
 
-const FriendItem = ({ friendItem }: { friendItem: Friend }) => {
-  const authenticatedUser = useSelector<Store, User>((store) => store.user)
+export const FriendItem = ({ friendItem }: { friendItem: User }) => {
   const { userInPreview } = useSelector<Store, UI>((store) => store.ui)
-
   const dispatch = useDispatch()
-
-  const friend = useMemo(() => {
-    if (friendItem.requester._id === authenticatedUser._id)
-      return friendItem.recipient
-    else return friendItem.requester
-  }, [friendItem.requester, friendItem.recipient, authenticatedUser._id])
+  const authenticatedUser = useSelector<Store, User>((store) => store.user)
 
   const handleMessageButtonClick = useHandleMessageButtonClick([
     authenticatedUser._id,
-    friend._id,
+    friendItem._id,
   ])
-
-  if (authenticatedUser._id === friend._id) return null
 
   return (
     <div
       onClick={() => {
-        if (userInPreview?._id === friend._id) return
+        if (userInPreview?._id === friendItem._id) return
         dispatch(removeUserInPreview())
         setTimeout(() => {
-          dispatch(updateUserInPreview(friend))
+          dispatch(updateUserInPreview(friendItem))
         }, 150)
       }}
       className={`w-full flex items-center ${
-        userInPreview?._id === friend._id
+        userInPreview?._id === friendItem._id
           ? 'bg-secondary-darkest/50'
           : 'bg-primary-default'
       }`}
@@ -52,15 +43,15 @@ const FriendItem = ({ friendItem }: { friendItem: Friend }) => {
           <Avatar
             width={49}
             height={49}
-            src={friend.profileImage?.path}
+            src={friendItem.profileImage?.path}
             alt={''}
           />
         </div>
         <div className='basis-auto flex grow justify-between items-center py-3 border-t border-t-contrast-secondary/20'>
           <div>
-            <div className='text-contrast-strong text-base'>{`${friend.nickName}`}</div>
+            <div className='text-contrast-strong text-base'>{`${friendItem.nickName}`}</div>
             <div className='text-contrast-secondary text-sm font-normal whitespace-nowrap flex relative w-full h-[20px]'>
-              {`${friend.firstName} ${friend.lastName}`}
+              {`${friendItem.firstName} ${friendItem.lastName}`}
             </div>
           </div>
           <MessageButton
@@ -77,6 +68,18 @@ const FriendItem = ({ friendItem }: { friendItem: Friend }) => {
   )
 }
 
+const Friendship = ({ friendItem }: { friendItem: Friend }) => {
+  const authenticatedUser = useSelector<Store, User>((store) => store.user)
+
+  const friend = useMemo(() => {
+    if (friendItem.requester._id === authenticatedUser._id)
+      return friendItem.recipient
+    else return friendItem.requester
+  }, [friendItem.requester, friendItem.recipient, authenticatedUser._id])
+
+  if (authenticatedUser._id === friend._id) return null
+  return <FriendItem friendItem={friend} />
+}
 export default function FriendsList() {
   const { friends } = useSelector<Store, FriendsState>((store) => store.friends)
   if (friends.length <= 0) return null
@@ -84,7 +87,7 @@ export default function FriendsList() {
     <>
       <>
         {friends.map((friend) => (
-          <FriendItem friendItem={friend} key={friend._id} />
+          <Friendship friendItem={friend} key={friend._id} />
         ))}
       </>
     </>

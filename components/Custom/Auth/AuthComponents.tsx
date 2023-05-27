f8ff6f2f8dcc33f80ce72b1dd0b3ca237083d908
exploitenomah <iksploiting@gmail.com>
2023-05-27 05:54:43 +0100

    add loading state

--- components/Custom/Auth/AuthComponents.tsx
+++ components/Custom/Auth/AuthComponents.tsx
@@ -7,7 +7,7 @@ import { InputHTMLAttributes, useMemo, useState, useEffect } from 'react'
 export default function AuthLoader() {
   return (
     <>
-      <div className='absolute top-0 left-0 w-full h-full bg-primary-darkest/80 z-40'>
+      <div className='absolute top-0 left-0 w-full h-full bg-primary-darkest/20 z-40'>
         <div className='flex justify-center items-center max-w-[30px] h-full mx-auto'>
           <Loader className='animate-spin text-white' />
         </div>

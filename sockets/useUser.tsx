36c83ea83e9be7019622fd9ca6d3c3b264f8b9ad
exploitenomah <iksploiting@gmail.com>
2023-05-29 13:35:20 +0100

    refactor

--- sockets/useUser.tsx
+++ sockets/useUser.tsx
@@ -2,15 +2,17 @@ import { Store } from '@store/index'
 import { Auth } from '@store/auth/initialState'
 import { addAppAlert } from '@store/notifications/slice'
 import { updateLoading } from '@store/ui/slice'
-import { useEffect, useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { io } from 'socket.io-client'
 import useHandlers from '@hooks/useHandlers'
 import { userEvents } from '@store/user/initialState'
 import { userActions } from '@store/user/slice'
 import { updateUser } from '@store/friends/slice'
+import useHandleConnection from '@hooks/useHandleConnection'
 
 export default function useUser() {
+  const { handleConnect, handleDisconnect } = useHandleConnection()
   const { token } = useSelector<Store, Auth>((store) => store.auth)
 
   const userSocket = useMemo(
@@ -34,44 +36,31 @@ export default function useUser() {
       }
     })
 
-    userSocket.io.on('reconnect', (attempt) => {
-      console.error(`${attempt} ==> at root socket`)
-      // if (onError) onError(attempt)
-    })
+    userSocket.on('connect', () => handleConnect())
 
-    userSocket.io.on('reconnect_attempt', (attempt) => {
-      // ...
-      console.log(attempt)
-    })
+    userSocket.io.on('reconnect', (_attempt) => handleConnect())
 
-    userSocket.io.on('reconnect_error', (error) => {
-      // ...
-      console.log(error)
-    })
+    userSocket.io.on('reconnect_attempt', (_attempt) => handleDisconnect())
 
-    userSocket.io.on('reconnect_failed', () => {
-      // ...
-      console.log('reconnect failed')
-    })
+    userSocket.io.on('reconnect_error', (_error) => handleDisconnect())
 
-    userSocket.on('connect_error', (err) => {
-      dispatch(addAppAlert({
-        message: 'Something went wrong!', variant: 'error',
-        id: Math.random()
-      }))
+    userSocket.io.on('reconnect_failed', () => handleDisconnect())
+
+    userSocket.io.on('error', (_err) => handleDisconnect())
+
+    userSocket.on('disconnect', handleDisconnect)
+
+    userSocket.on('connect_error', (_err) => {
+      dispatch(
+        addAppAlert({
+          message: 'Something went wrong!',
+          variant: 'error',
+          id: Math.random(),
+        }),
+      )
       userSocket.connect()
     })
-    userSocket.io.on('error', (err) => {
-      console.log(err.message)
-    })
 
-    userSocket.on('connect', () => {
-      console.log('user socket connected')
-    })
-
-    userSocket.on('disconnect', (reason) => {
-      console.log('root socket disconnected', reason)
-    })
     userSocket.on('error', (msg) => {
       dispatch(
         addAppAlert({
@@ -81,23 +70,20 @@ export default function useUser() {
         }),
       )
       dispatch(updateLoading(false))
-      console.log(msg)
     })
 
     return () => {
-      userSocket.off('signup', (data) => console.log(`${data} off`))
-      userSocket.off('login', (data) => console.log(`${data} off`))
-      userSocket.off('connect', () => console.log(`connect off`))
-      userSocket.off('disconnect', (reason) => console.log(`${reason} off`))
-      userSocket.io.off('error', (msg) => console.log(`${msg} off`))
-      userSocket.io.off('reconnect', (data) => console.log(`${data} off`))
-      userSocket.io.off('reconnect_attempt', (data) =>
-        console.log(`${data} off`),
-      )
-      userSocket.io.off('reconnect_failed', () => console.log(`$ off`))
-      userSocket.io.off('reconnect_error', (data) => console.log(`${data} off`))
+      userSocket.off('signup', () => {})
+      userSocket.off('login', () => {})
+      userSocket.off('connect', () => {})
+      userSocket.off('disconnect', () => {})
+      userSocket.io.off('error', () => {})
+      userSocket.io.off('reconnect', () => {})
+      userSocket.io.off('reconnect_attempt', () => {})
+      userSocket.io.off('reconnect_failed', () => {})
+      userSocket.io.off('reconnect_error', () => {})
     }
-  }, [dispatch, userSocket])
+  }, [dispatch, handleConnect, handleDisconnect, userSocket])
 
   return userSocket
 }

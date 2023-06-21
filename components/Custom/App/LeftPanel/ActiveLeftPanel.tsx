ebed68b227fed6dc1ebf2717897fdb30f22ed367
exploitenomah <iksploiting@gmail.com>
2023-06-21 10:59:36 +0100

    setup conversation search

--- components/Custom/App/LeftPanel/ActiveLeftPanel.tsx
+++ components/Custom/App/LeftPanel/ActiveLeftPanel.tsx
@@ -4,18 +4,49 @@ import { ConversationsState } from '@store/conversations/initialState'
 import { Store } from '@store/index'
 import { useDispatch, useSelector } from 'react-redux'
 import { updateSearchText } from '@store/conversations/slice'
+import useDebounce from '@hooks/useDebounce'
+import { searchLimit } from '@store/search/initialState'
+import { toggleLoading } from '@store/search/slice'
+import useSearch from '@sockets/useSearch'
+import {
+  useGetSearchedMessagesToDisplay,
+  useSearchConversations,
+} from '@hooks/conversations'
 
 const ConversationsSearch = () => {
   const { searchText } = useSelector<Store, ConversationsState>(
     (store) => store.conversations,
   )
+  const searchedMessages = useGetSearchedMessagesToDisplay()
   const dispatch = useDispatch()
+  const searchSocket = useSearch()
+  const searchedConversations = useSearchConversations()
+  const search = useDebounce((searchText) => {
+    if (
+      searchedConversations.length === 0 &&
+      searchedMessages.length === 0 &&
+      searchText.length > 1
+    ) {
+      searchSocket.emit('searchMessages', {
+        search: searchText,
+        limit: searchLimit,
+        page: 1,
+      })
+    } else {
+      dispatch(toggleLoading(false))
+    }
+  }, 5000)
+
   return (
     <SearchBar
       inputProps={{
         placeholder: 'Search',
         value: searchText,
-        onChange: (e) => dispatch(updateSearchText(e.target.value)),
+        onChange: (e) => {
+          dispatch(updateSearchText(e.target.value))
+          search(e.target.value)
+          dispatch(toggleLoading(true))
+        },
       }}
     />
   )

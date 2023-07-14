263862cf3ee93beb80dae059d25e8e7f48d9508d
exploitenomah <iksploiting@gmail.com>
2023-07-14 04:56:13 +0100

    linkify messages with links

--- utils/index.ts
+++ utils/index.ts
@@ -30,3 +30,11 @@ export const isSubString = (str: string, match: string) => {
 export const isExactSubString = (str: string, match: string) => {
   return str.startsWith(match.trim()) || str.includes(match.trim())
 }
+
+export function linkify(text: string) {
+  var urlRegex =
+    /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=_|!:,.;]*[-A-Z0-9+&@#\/%=_|])/gi
+  return text.replace(urlRegex, function (url) {
+    return `<span style="color:#74c0ff;cursor:pointer;"><a target='__blank' href=${url}>${url}</a></span>`
+  })
+}

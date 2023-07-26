511e6d6ec9386ea29d609c36a5c1854e4df4ffbc
exploitenomah <iksploiting@gmail.com>
2023-07-26 12:58:32 +0100

    write tests for signup form

--- cypress/e2e/index.cy.ts
+++ cypress/e2e/index.cy.ts
@@ -16,3 +16,27 @@ describe('Non Authenticated App Screen', () => {
     cy.get('[data-test-id="login-modal"]')
   })
 })
+describe('Signup Modal', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3001')
+    cy.clearAllLocalStorage()
+  })
+  after(() => {
+    cy.request('http://localhost:3000/api/v1/db/reset')
+  })
+  it('Signup form submit button is disabled if inputs are empty', () => {
+    cy.get('[data-test-id="primary-cta"]').click()
+    cy.get('[data-test-id="submit-button"]').should('be.disabled')
+  })
+  it('Signup modal allows input and submits', () => {
+    cy.get('[data-test-id="primary-cta"]').click()
+    cy.get('[data-test-id="first-name-input"]').type('cypress')
+    cy.get('[data-test-id="last-name-input"]').type('tests')
+    cy.get('[data-test-id="nick-name-input"]').type('tests')
+    cy.get('[data-test-id="email-input"]').type('cypresstests@gmail.com')
+    cy.get('[data-test-id="password-input"]').type('dev1234')
+    cy.get('[data-test-id="confirm-password-input"]').type('dev1234')
+    cy.get('[data-test-id="submit-button"]').should('not.be.disabled').click()
+    cy.get('[data-test-id="primary-cta"]').should('not.exist')
+  })
+})

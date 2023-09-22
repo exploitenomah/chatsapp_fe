290efd2eebfe8beb336d543930480b226a69d92a
exploitenomah <iksploiting@gmail.com>
2023-09-22 14:18:54 +0100

    write tests

--- components/Custom/LeftDrawer.tsx
+++ components/Custom/LeftDrawer.tsx
@@ -1,20 +1,22 @@
-import { ReactNode, useMemo } from 'react'
+import { HTMLAttributes, ReactNode, useMemo } from 'react'
 
 export default function LeftDrawer({
   children,
   show,
   zIndex,
+  ...rest
 }: {
   children: ReactNode | ReactNode[]
   show: boolean
   zIndex: string
-}) {
+} & HTMLAttributes<HTMLDivElement>) {
   return (
     <>
       <div
         className={`absolute h-full w-full bg-primary-default transition-all duration-300 ${
           show ? 'translate-x-0' : '-translate-x-full '
         } ${zIndex}`}
+        {...rest}
       >
         {children}
       </div>
